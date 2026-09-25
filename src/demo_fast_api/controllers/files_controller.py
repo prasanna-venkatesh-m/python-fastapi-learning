c01039951c016eb440b5c696cb2fa401877ccfcb
Prasanna <prasannavenkatesh.m@arisecapital.in>
2026-09-25 19:25:43 +0530

    Feat : Authentication in Files Controller

--- src/demo_fast_api/controllers/files_controller.py
+++ src/demo_fast_api/controllers/files_controller.py
@@ -1,10 +1,11 @@
 from fastapi import APIRouter, File, Form, UploadFile, Depends
 from demo_fast_api.services.file_service import FileService
 from demo_fast_api.dto.enums.file_type_enum import FileType
+from demo_fast_api.utils.dependencies import authenticate, authorize
 
-router = APIRouter(prefix="/files", tags=["Files"])
+router = APIRouter(prefix="/files", tags=["Files"], dependencies=[Depends(authenticate)])
 
-@router.post(path="/upload-file" )
+@router.post(path="/upload-file", dependencies=[Depends(authorize(["ADMIN"]))])
 async def upload_files(
     fileType: FileType = Form(...), file: UploadFile = File(...),
     fileService : FileService = Depends(FileService)
